c6aa4f978a76d57aa8c7d57fc2c80369dcd6dc93
FcaHJ <127349236+FcaHJ@users.noreply.github.com>
2023-05-11 22:06:05 -0400

    Se creo el archivo JS

---
+++ Evaluacion/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("formulario").addEventListener('submit', validarFormulario)
+})
+
+function validarFormulario(){
+    evento.preventDefault();
+}
